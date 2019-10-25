a3d32ce2a9aa90c0b9fa4a0a379cc1c8dc9e6177
Dodi Cristian-Dumitru <cdodi907@gmail.com>
2019-10-25 13:29:08 +0300

    technical setup

---
+++ src/main/java/com/utm/udrive/UdriveApplication.java
@@ -0,0 +1,13 @@
+package com.utm.udrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdriveApplication.class, args);
+	}
+
+}
